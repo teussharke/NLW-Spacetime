18c53aff18e9fdf3f604b00159a78519f5ee5a1e
Mateus Oliveira <109149179+teussharke@users.noreply.github.com>
2023-05-18 12:10:08 -0300

    att

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -1,8 +1,62 @@
+import { User } from 'lucide-react'
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        {/* stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="nlw-spacetime" />
+          <div className="max-w[420px] space-y-1">
+            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+          <a href="" className="inline-block">
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora!
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
