ecf128f5cb6e6ed12108c6c8a4da5d73489a6c25
Mateus Oliveira <109149179+teussharke@users.noreply.github.com>
2023-05-18 13:00:27 -0300

    att

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -32,16 +32,32 @@ export default function Home() {
         <div className="space-y-5">
           <Image src={nlwLogo} alt="nlw-spacetime" />
           <div className="max-w[420px] space-y-1">
-            <h1 className="mt-5 text-5xl font-bold leading-tight text-gray-50">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
               Sua cápsula do tempo
             </h1>
-            <p>
+            <p className=" text-lg leading-relaxed">
               Colecione momentos marcantes da sua jornada e compartilhe (se
               quiser) com o mundo!
             </p>
           </div>
-          <a href="" className="inline-block">
+          <a
+            href=""
+            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+          >
             CADASTRAR LEMBRANÇA
+          </a>
+        </div>
+
+        {/* Copyright */}
+        <div className="text-sm leading-relaxed text-gray-200">
+          Feito com 💜 no NLW da{' '}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="underline hover:text-gray-100"
+            href="https://rocketseat.com.br"
+          >
+            Rocketseat
           </a>
         </div>
       </div>
