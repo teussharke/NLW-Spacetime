4a0c89bbfddd61cb30059d336d433994f9cb9f4d
Mateus <teussharke@gmail.com>
2023-07-05 17:51:11 -0300

    att

--- aulas/mobile/app/index.tsx
+++ aulas/mobile/app/index.tsx
@@ -1,22 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-
-import { styled } from 'nativewind'
-import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
 
-const StyledStripes = styled(Stripes)
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -26,13 +15,9 @@ const discovery = {
 }
 
 export default function App() {
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
+  const router = useRouter()
 
-  const [request, response, signInWithGithub] = useAuthRequest(
+  const [, response, signInWithGithub] = useAuthRequest(
     {
       clientId: '456f74524295cbeb4b3a',
       scopes: ['identity'],
@@ -43,37 +28,49 @@ export default function App() {
     discovery,
   )
 
+  async function handleGithubOAuthCode(code: string) {
+    console.log(code)
+    try {
+      const response = await api.post('/register', {
+        code,
+      })
+
+      console.log(response.data)
+      const { token } = response.data
+
+      await SecureStore.setItemAsync('token', token)
+
+      router.push('/memories')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   useEffect(() => {
     // console.log(
+    //   'response',
     //   makeRedirectUri({
     //     scheme: 'nlwspacetime',
     //   }),
     // )
+
     if (response?.type === 'success') {
       const { code } = response.params
+
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 bg-gray-900 px-12 py-10"
-      imageStyle={{ position: 'absolute', left: '-100%' }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
 
         <View className="space-y-2">
-          <Text className="text=center font-title text-2xl leading-tight text-gray-50">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
             Sua cápsula do tempo
           </Text>
-          <Text className="texte-base text-center font-body leading-relaxed text-gray-100">
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
             Colecione momentos marcantes da sua jornada e compartilhe (se
             quiser) com o mundo!
           </Text>
@@ -84,7 +81,7 @@ export default function App() {
           className="rounded-full bg-green-500 px-5 py-2"
           onPress={() => signInWithGithub()}
         >
-          <Text className="font-alt text-sm uppercase text-black ">
+          <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
           </Text>
         </TouchableOpacity>
@@ -93,8 +90,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" />
-    </ImageBackground>
+    </View>
   )
 }
